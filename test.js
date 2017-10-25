2d65d7ce9939502ea3dbcf2f727513b94beb2adc
Rajan Davis <rajangdavis@gmail.com>
2017-10-25 01:40:08 -0700

    added functionality for the queryResults object. Gotta figure out how to do the queryresultsset in javascript and then do it in python...

--- test.js
+++ test.js
@@ -2,23 +2,21 @@ const OSCNode = require('./lib/OSCNode.js');
 const env = process.env;
 
 var rn_client = OSCNode.Client({
-  username: env['OSC_ADMIN'],
-  password: env['OSC_PASSWORD'],
-  interface: env['OSC_SITE'],
-  demo_site:true
+	username: env['OSC_ADMIN'],
+	password: env['OSC_PASSWORD'],
+	interface: env['OSC_SITE'],
+	demo_site:true
 });
 
-var contactsQuery = `SELECT * 
-					 FROM contacts
-					 WHERE createdTime > '2005-01-10T04:00:00Z'`
+var contactsQuery = `DESCRIBE`
 
 var options = {
 	client: rn_client,
 	query: contactsQuery
 }
 
-OSCNode.QueryResults.query(options,(err,response,body) =>{
-	console.log(err);
-	console.log(response);
-	console.log(body);
-})
+OSCNode.QueryResults.query(options,(err,results) =>{
+	results.map(function(result){
+		console.log(result.Name);
+	})
+});
