4c5128bf87aa7f7665ccc4c71800a808c51376af
rajangdavis <rajangdavis@gmail.com>
2018-06-01 18:17:05 -0700

    updated tests for the config object as it has been updated for how it works

--- tests/lib/Config.js
+++ tests/lib/Config.js
@@ -1,119 +1,128 @@
-// var assert = require('chai').assert;
-// var config = require('../../lib/Config.js');
-// var client = require('../../lib/Client.js');
+var assert = require('chai').assert;
+var config = require('../../lib/Config.js');
+var client = require('../../lib/Client.js');
 
-// describe('connect.headersHash', function(){ 
+describe('connect.optionsFinalize', function(){ 
 
-// 	var oscConfigHeaders = {
-// 		username: 'username',
-// 		password: 'password',
-// 		interface: 'interface',
-// 		no_ssl_verify: true,
-// 		suppress_rules: true
-// 	}
+	var rnClient = new client({
+		username: 'username',
+		password: 'password',
+		interface: 'interface',
+		no_ssl_verify: true,
+		suppress_rules: true
+	});
 
-// 	var rnClient = new client(oscConfigHeaders); 
-// 	var oncHeaders = config.headersHash(rnClient,true);
+	var options = {
+		client: rnClient
+	}
 
-// 	it('should take a client object and return a hash of header settings; ' +
-// 		'the "suppress_rules" setting should return ' +
-// 		'headers["OSvC-CREST-Suppress-All"] as true',function(){
-// 		assert.strictEqual(oncHeaders["OSvC-CREST-Suppress-All"],true);
-// 	});
+	var finalOptions = config.optionsFinalize("post",options,true);
 
-// 	it('should take a boolean param for making PATCH requests; ' +
-// 		'headers["X-HTTP-Method-Override"] as "PATCH"',function(){
-// 		assert.strictEqual(oncHeaders["X-HTTP-Method-Override"],'PATCH');
-// 	});
+	it('should take an HTTP verb and set the method property to match that verb',function(){
+		assert.strictEqual(finalOptions.method,"post");
+	});
 
-// 	var defaultConfigHeaders = {
-// 		username: 'username',
-// 		password: 'password',
-// 		interface: 'interface'
-// 	}
+	it('should take a client object and return a hash of options settings; ' +
+		'the "suppress_rules" setting should return ' +
+		'headers["OSvC-CREST-Suppress-All"] as true',function(){
+		assert.strictEqual(finalOptions.headers["OSvC-CREST-Suppress-All"],true);
+	});
 
-// 	var defaultClient = new client(defaultConfigHeaders); 
-// 	var defaultHeaders = config.headersHash(defaultClient);
+	it('should take a boolean param for making PATCH requests; ' +
+		'headers["X-HTTP-Method-Override"] as "PATCH"',function(){
+		assert.strictEqual(finalOptions.headers["X-HTTP-Method-Override"],'PATCH');
+	});
 
-// 	it('should have headers set to undefined as a default',function(){
-// 		assert.strictEqual(defaultHeaders["OSvC-CREST-Suppress-All"],undefined);
-// 		assert.strictEqual(defaultHeaders["X-HTTP-Method-Override"],undefined);
-// 	});
+	var defaultConfigHeaders = new client({
+		username: 'username',
+		password: 'password',
+		interface: 'interface'
+	});
 
-// });
+	var defaultOptions = {
+		client: rnClient
+	}
 
-// describe('config.clientUrl',function(){
+	var defaultHeaders = config.optionsFinalize("get", defaultOptions);
 
-// 	var oscConfigURL = {
-// 		username: 'username123',
-// 		password: 'password456',
-// 		interface: 'interface789',
-// 		demo_site: true,
-// 		version: 'v1.4',
-// 		no_ssl_verify: true,
-// 		suppress_rules: true
-// 	}
+	it('should have headers set to undefined as a default',function(){
+		assert.strictEqual(defaultHeaders["OSvC-CREST-Suppress-All"],undefined);
+		assert.strictEqual(defaultHeaders["X-HTTP-Method-Override"],undefined);
+	});
 
-// 	var rnClient = new client(oscConfigURL); 
+	var oscConfigURL = {
+		client: new client({
+			username: 'username123',
+			password: 'password456',
+			interface: 'interface789',
+			demo_site: true,
+			version: 'v1.4',
+			no_ssl_verify: true,
+			suppress_rules: true
+		}),
+		url: "incidents"
+	}
+
+	var osvcURL = config.optionsFinalize("get", oscConfigURL);	
+
+	it('should always use "https" and "/services/rest/connect" in the url',function(){
+		assert.match(osvcURL.url,/\/services\/rest\/connect/);
+		assert.match(osvcURL.url,/https/);
+	});
+
+	it('should take a client object and change the url to include "rightnowdemo" if '+
+		'the "demo_site" setting is set to true',function(){
+		assert.match(osvcURL.url,/rightnowdemo\.com/);
+	});
+
+	it('should take a username,password,and interface and change the url to include the interface',function(){
+		assert.match(osvcURL.url,/interface789/);
+	});
+
+	it('should take a username,password and return an authorization header for basic auth',function(){
+		let encryptedCred = "Basic " + Buffer.from("username123:password456").toString("base64");
+		assert.strictEqual(osvcURL.headers["Authorization"], encryptedCred);
+	});	
+
+
+	it('should take a client object and change the url to include a different verions if '+
+		'the "version" setting is changed',function(){
+		assert.match(osvcURL.url,/v1\.4/);
+	});
+
+	it('should take a resource URL and change the url',function(){
+		assert.match(osvcURL.url,/incidents/);
+	});
 	
-// 	var oncURL = config.clientUrl(rnClient,"incidents");
-
-// 	it('should always use "https" and "/services/rest/connect" in the url',function(){
-// 		assert.match(oncURL,/\/services\/rest\/connect/);
-// 		assert.match(oncURL,/https/);
-// 	});
-
-// 	it('should take a client object and change the url to include "rightnowdemo" if '+
-// 		'the "demo_site" setting is set to true',function(){
-// 		assert.match(oncURL,/rightnowdemo\.com/);
-
-// 	});
-
-// 	it('should take a username,password,and interface and change the url to include the username',function(){
-// 		assert.match(oncURL,/username123/);
-// 		assert.match(oncURL,/password456/);
-// 		assert.match(oncURL,/interface789/);
-// 	});
-
-
-// 	it('should take a client object and change the url to include a different verions if '+
-// 		'the "version" setting is changed',function(){
-// 		assert.match(oncURL,/v1\.4/);
-// 	});
-
-// 	it('should take a resource URL and change the url',function(){
-// 		assert.match(oncURL,/incidents/);
-// 	});
-	
-// 	it('should equal "https://username123:password456@interface789.rightnowdemo.com/services/rest/connect/v1.4/incidents"',function(){
-// 		assert.strictEqual(oncURL,"https://username123:password456@interface789.rightnowdemo.com/services/rest/connect/v1.4/incidents")
-// 	});
+	it('should equal "https://interface789.rightnowdemo.com/services/rest/connect/v1.4/incidents"',function(){
+		assert.strictEqual(osvcURL.url,"https://interface789.rightnowdemo.com/services/rest/connect/v1.4/incidents")
+	});
 
 	
-// 	var defaultConfigURL = {
-// 		username: 'username123',
-// 		password: 'password456',
-// 		interface: 'interface789'
-// 	}
+	var defaultConfigURL = {
+		client: client({
+			username: 'username123',
+			password: 'password456',
+			interface: 'interface789'
+		})
+	}
+	
+	var defaultResourceUrl = config.optionsFinalize("get", defaultConfigURL);
 
-// 	var defaultClient = new client(defaultConfigURL);
+	it('should take a not match incidents if the resource URL is not specified',function(){
+		assert.notMatch(defaultResourceUrl.url,/incidents/);
+	});
 
-// 	var defaultResourceUrl = config.clientUrl(defaultClient);
+	it('"version" should be "v1.3/" if not specified',function(){
+		assert.match(defaultResourceUrl.url,/v1\.3\//);
+	});
 
-// 	it('should take a not match incidents if the resource URL is not specified',function(){
-// 		assert.notMatch(defaultResourceUrl,/incidents/);
-// 	});
+	it('"custhelp" domain will be used if not specified',function(){
+		assert.match(defaultResourceUrl.url,/custhelp.com/);
+	});
 
-// 	it('"version" should be "v1.3/" if not specified',function(){
-// 		assert.match(defaultResourceUrl,/v1\.3\//);
-// 	});
+	it('should equal "https://interface789.custhelp.com/services/rest/connect/v1.3/"',function(){
+		assert.strictEqual(defaultResourceUrl.url,"https://interface789.custhelp.com/services/rest/connect/v1.3/")
+	});
 
-// 	it('"custhelp" domain will be used if not specified',function(){
-// 		assert.match(defaultResourceUrl,/custhelp.com/);
-// 	});
-
-// 	it('should equal "https://username123:password456@interface789.custhelp.com/services/rest/connect/v1.3/"',function(){
-// 		assert.strictEqual(defaultResourceUrl,"https://username123:password456@interface789.custhelp.com/services/rest/connect/v1.3/")
-// 	});
-
-// });
+});
