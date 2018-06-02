008ab1e856390cf6d6d0e6cd3dc297d42af10778
rajangdavis <rajangdavis@gmail.com>
2018-06-01 20:14:05 -0700

    removing the base level object tests

--- tests/lib/OSvCNode.js
+++ tests/lib/OSvCNode.js
@@ -1,18 +1,18 @@
-var assert = require('chai').assert;
+// var assert = require('chai').assert;
 
-var osvcNode = require('./../../lib/OSvCNode.js');
+// var osvcNode = require('../../lib/OSvCNode.js');
 
-describe('OSvCNode module', function(){
+// describe('OSvCNode module', function(){
 
 
-	it('should have Connect, Client, QueryResults, QueryResultsSet, and AnalyticsReportResults properties that mostly objects',function(){
+// 	it('should have Connect, Client, QueryResults, QueryResultsSet, and AnalyticsReportResults properties that mostly objects',function(){
 
-		assert.strictEqual(typeof osvcNode.Connect, "object");
-		assert.strictEqual(typeof osvcNode.Client, "function");
-		assert.strictEqual(typeof osvcNode.QueryResults, "object");
-		assert.strictEqual(typeof osvcNode.QueryResultsSet, "object");
-		assert.strictEqual(typeof osvcNode.AnalyticsReportResults, "object");
+// 		assert.strictEqual(typeof osvcNode.Connect, "object");
+// 		assert.strictEqual(typeof osvcNode.Client, "function");
+// 		assert.strictEqual(typeof osvcNode.QueryResults, "object");
+// 		assert.strictEqual(typeof osvcNode.QueryResultsSet, "object");
+// 		assert.strictEqual(typeof osvcNode.AnalyticsReportResults, "object");
 
-	});
+// 	});
 
-});
+// });
